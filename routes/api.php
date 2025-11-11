7e9553d91cddd692521363562d5c26e5a006ff32
Lucas Cerqueira <lucasvscs@outlook.com>
2025-11-11 02:10:05 -0300

    feat: setup new routes (articles and categories)

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,27 @@
 <?php
 
-use App\Http\Controllers\Api\SourceController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SourceController;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\CategoryController;
 
-Route::prefix('sources')->group(function () {
-    Route::post('toggle-active/{id}', [SourceController::class, 'toggleActive']);
-    Route::get('active', [SourceController::class, 'getActive']);
+Route::prefix('v1')->group(function () {
+    // Sources
+    Route::prefix('sources')->group(function () {
+        Route::post('toggle-active/{id}', [SourceController::class, 'toggleActive']);
+        Route::get('active', [SourceController::class, 'getActive']);
+    });
+    Route::apiResource('sources', SourceController::class);
+
+    // Articles
+    Route::prefix('articles')->group(function () {
+        Route::get('latest', [ArticleController::class, 'latest']);
+        Route::get('source/{sourceId}', [ArticleController::class, 'bySource']);
+        Route::get('category/{categoryId}', [ArticleController::class, 'byCategory']);
+        Route::get('authors', [ArticleController::class, 'authors']);
+    });
+    Route::apiResource('articles', ArticleController::class)->only(['index', 'show']);
+
+    // Categories
+    Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
 });
-Route::apiResource('sources', SourceController::class);
